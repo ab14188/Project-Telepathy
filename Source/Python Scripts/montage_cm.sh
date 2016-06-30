08276f05a440268f9bfee2c429458f00c7eaec6e
Anne-Claire Bourland <ab14188@it025993.users.bris.ac.uk>
2016-06-30 11:33:40 +0100

    montage_cm.sh

---
+++ Source/Python Scripts/montage_cm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Creating confusion matrices montage"
+i=0
+mkdir tmp
+for d in ./user_study/results/*
+do 
+	echo $d
+	for file in $d$"/CM/*"
+	do 
+		echo $file
+		montage $file -geometry 800x600 -tile 2x1 tmp/tmp"$i".pdf
+		let "i += 1"
+done
+
+convert tmp/* cm_montage.pdf
+
+rm -r tmp
